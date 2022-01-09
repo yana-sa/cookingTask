b6545fffd10a971b1aafce7c1bfc5f7c29a66028
Yana Samagala <yanasamagala@gmail.com>
2022-01-09 23:45:58 +0200

    add migrations

---
+++ database/migrations/2022_01_09_204808_create_events_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('class_id');
+            $table->string('name', 64);
+            $table->dateTime('starts_at');
+        });
+
+        Schema::table('events', function(Blueprint $table) {
+            $table->foreign('class_id')->references('id')->on('classes')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
